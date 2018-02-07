470cd55bc9cf3ea7b4ce15779351be2c66cac43d
Kosmarjus <marjus@gmail.com>
2018-02-07 18:25:01 +0200

    changing to current experiment base

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-let a = document.getElementById("testing");
-a.onmouseout = mouseout;
-a.onmouseover = mouseover;
-a.onmousedown = mousedown;
-a.onmouseup = mouseup;
+/*jshint esversion: 6 */
 
 
+//susideliojame salis i objektus su reikalingais parametrais objekte
+const salys = {
+    ltu: {
+        pavadinimas: 'Lietuva',
+        gyventojai: 2.8,
+        plotas: 65300
+    },
+    ind: {
+        pavadinimas: 'Indija',
+        gyventojai: 1342.5,
+        plotas: 3287590
+    },
+    chn: {
+        pavadinimas: 'Kinija',
+        gyventojai: 1388.2,
+        plotas: 9596960
+    },
+    usa: {
+        pavadinimas: 'Jungtinės Amerikos Valstijos',
+        gyventojai: 326.4,
+        plotas: 9826630
+    }
+};
 
+//sutrumpinimas
+let pasirinkimas = document.getElementById("pasirinkimas");
 
-function mouseout() {
-    let writing = document.getElementById("testing");
-    writing.innerHTML = 'whaaaaat';
-}
+//priskiriame funkcija pasikeitus musu dropdown meniu pasirinkimui
+pasirinkimas.onchange =  abrakadabra;
 
-function mouseover() {
-    let writing = document.getElementById("testing");
-    writing.innerHTML = 'what now?';
-}
+//funkcija su event parametru
+function abrakadabra(choice) {
 
-function mousedown() {
-    let writing = document.getElementById("testing");
-    writing.innerHTML = 'NOOOOOOO';
-    writing.style.backgroundColor = "Yellow";
-    writing.style.alignSelf = "flex-end";
-}
+    //priskiriame evento verte kintamam
+   let userChoice = choice.target.value;
 
-function mouseup() {
-    let writing = document.getElementById("testing");
-    writing.innerHTML = 'YSSSSSSSSSSS';
-    setTimeout(mouseout, 1500);
-    writing.style.backgroundColor = "";
-    writing.style.alignSelf = "";
-    
+ //jei nieko nepasirinkom - slepiam viska
+    if (userChoice == "") {
+        document.getElementById('textOutput').style.opacity=0;
 
+ 
+    } else {
+        document.getElementById('textOutput').style.opacity=1;        //jei kazka pasirinkom - grazinam teksto matomuma
+        //ziurim objekto atitikmeni, imam atitinkama parametra
+        document.getElementById('salis').innerHTML = salys[userChoice].pavadinimas; 
+        document.getElementById('gyventojai').innerHTML = salys[userChoice].gyventojai.toLocaleString('lt-LT', {maximumFractionDigits: 2});
+        document.getElementById('plotas').innerHTML = salys[userChoice].plotas.toLocaleString('lt-LT', {maximumFractionDigits: 2});
+        document.getElementById('plotasGyventojui').innerHTML = (salys[userChoice].plotas / salys[userChoice].gyventojai).toLocaleString('lt-LT', {maximumFractionDigits: 2});
+    }
 }
