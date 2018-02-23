cecefbcacc3129206c51e61f70a28de84e8359c1
Kosmarjus <marjus@gmail.com>
2018-02-23 21:31:26 +0200

    with images

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ function uuidv4() {
 
 //f-ja irasu issaugojimui i localstorage
 function storeInput() {
+
     let inputName = document.getElementById('input').value;
     let inputProperty1 = document.getElementById('property1').value;
     let inputProperty2 = document.getElementById('property2').value;
+    let img = document.getElementById('img').files[0].name;
 
     var result = dataArray.filter(function (obj) { //f-ja objekto atrinkimui pagal dabartini ID
         return obj.id == targetID;
@@ -23,7 +25,8 @@ function storeInput() {
             id: uuidv4(),
             name: inputName,
             property1: inputProperty1,
-            property2: inputProperty2
+            property2: inputProperty2,
+            image: 'images/'+img
         });
 
     } else { //jei yra irasas - atnaujinam seno iraso propercius
@@ -31,6 +34,7 @@ function storeInput() {
         result.name = inputName;
         result.property1 = inputProperty1;
         result.property2 = inputProperty2;
+        result.image = 'images/'+img;
     }
 
     localStorage.setItem('dataArray', JSON.stringify(dataArray)); //stumiam i localstorage
@@ -45,10 +49,11 @@ function printInput() {
     for (const entry of dataArray) {
 
         printOut += `
-        <a class="containerBox" data-toggle="modal" href="#promptModal" onclick="populateModal()" id="${entry.id}"><div> 
+        <a class="containerBox" data-toggle="modal" href="#promptModal" onclick="populateModal('${entry.id}')"><div> 
         <ul> Pavadinimas:            ${entry.name}
             <li> Propertis1: ${entry.property1}</li>
             <li> Propertis2: ${entry.property2}</li>
+            <img src="${entry.image}">
         </ul>
         </div>
         </a>
@@ -62,7 +67,15 @@ function clearInputs() {
     document.getElementById('input').value = "";
     document.getElementById('property1').value = "";
     document.getElementById('property2').value = "";
+    document.getElementById('img').value = "";
     document.getElementById('deleteButton').style.visibility = 'hidden'; //tuo paciu paslepiam delete mygtuka jei nekoreguojam seno iraso
+    document.getElementById('showimage').style.visibility = 'hidden';
+}
+
+
+//funkcija seno irasyto ID istrynimui kai kuriamas naujas irasas
+function clearID() {
+    targetID = ''
 }
 
 // jei yra duomenu data arrayjuje = juos paparsinam ir atspausdinam per print f-ja
@@ -75,8 +88,8 @@ function render() {
 render();
 
 //f-ja modalinio lango uzpildymui
-function populateModal() {
-
+function populateModal(uniqueNo) { //panaudojam funkcijos parametro value atitikmens paieskai dataArray'uje
+    targetID = uniqueNo;
     dataArray = JSON.parse(localStorage.getItem('dataArray'));
 
     var result = dataArray.filter(function (obj) { //f-ja objekto atrinkimui pagal dabartini ID
@@ -86,17 +99,19 @@ function populateModal() {
     document.getElementById('input').value = result.name; //priskiriame inputams vertes is dataArray
     document.getElementById('property1').value = result.property1;
     document.getElementById('property2').value = result.property2;
-
+    // document.getElementById('img').value = result.image;
+    document.getElementById('showimage').src = result.image;
     document.getElementById('deleteButton').style.visibility = 'visible'; //redaguojant irasa - padarome mygtuka delete matoma
+    document.getElementById('showimage').style.visibility = 'visible';
 }
 
-//funkcija targetID paemimui
-function getID(e) {
-    targetID = e.target.id;
-}
-//targetID trigeriai
-document.querySelector('.main').addEventListener('mouseover', getID, false);
-document.querySelector('.main').addEventListener('focus', getID, true);
+// //funkcija targetID paemimui
+// function getID(e) {
+//     targetID = e.target.id;
+// }
+// //targetID trigeriai
+// document.querySelector('.main').addEventListener('mouseover', getID, false);
+// document.querySelector('.main').addEventListener('focus', getID, true);
 
 //trynimo mygtuko funkcija
 function deleteCard() {
@@ -114,6 +129,28 @@ function deleteCard() {
     $('#promptModal').modal('hide'); //slepiam modalini langa po mygtuko paspaudimo
     render(); //atnaujinam ekrana
 }
+
+
+function renderImage(input) {
+    if (input.files && input.files[0]) {
+
+        document.getElementById('showimage').style.visibility = 'visible';
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#showimage')
+                .attr('src', e.target.result)
+                .width(256)
+                .height(256);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
+
+
 
 //search filter tired of making it work with native JS, adapted some stack overflow JSON black magic
 $("#searchInput").on("keyup", function () {
